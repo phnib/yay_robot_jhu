0997774e0056cecde20ca0f9fcb74dcd2367b805
JuoTungChen <jjcc0704@gmail.com>
2024-07-18 22:52:41 -0400

    add phantom data to hl training

--- src/aloha_pro/aloha_scripts/constants_daVinci.py
+++ src/aloha_pro/aloha_scripts/constants_daVinci.py
@@ -7,7 +7,16 @@ DATASET_CONFIGS = {
     "base_chole_clipping_cutting": {
         "dataset_dir": os.path.join(DATA_DIR, "base_chole_clipping_cutting"),
         "num_episodes": 2500, # TODO: Adjust here the number of generated episode of the generated chole recordings
-        "incomplete_tissue_samples": ["tissue_1"], # Should not be used for HL policy training - good for LL and ML policy training 
+        "incomplete_tissue_samples": ["tissue_1", "phantom_1"], # Should not be used for HL policy training - good for LL and ML policy training 
+        "camera_names": ["endo_psm2", "left_img_dir", "right_img_dir", "endo_psm1"], 
+        "camera_file_suffixes": ["_psm2.jpg", "_left.jpg", "_right.jpg", "_psm1.jpg"],
+        "after_phase_offset": 10,
+        "before_phase_offset": 10,   
+    },
+    "phantom_chole": {
+        "dataset_dir": os.path.join(DATA_DIR, "phantom_chole"),
+        "num_episodes": 2500, # TODO: Adjust here the number of generated episode of the generated chole recordings
+        "incomplete_tissue_samples": [], # Should not be used for HL policy training - good for LL and ML policy training 
         "camera_names": ["endo_psm2", "left_img_dir", "right_img_dir", "endo_psm1"], 
         "camera_file_suffixes": ["_psm2.jpg", "_left.jpg", "_right.jpg", "_psm1.jpg"],
         "after_phase_offset": 10,
