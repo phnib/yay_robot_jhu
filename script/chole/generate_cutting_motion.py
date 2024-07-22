0d6f35f5e9c07c2bdb0247f6a148dec88a2f0d23
JuoTungChen <jjcc0704@gmail.com>
2024-07-22 13:32:09 -0400

    generate cutting motion for psm1_jaw_sp as well

--- script/chole/generate_cutting_motion.py
+++ script/chole/generate_cutting_motion.py
@@ -40,38 +40,80 @@ def generate_cutting_motion(csv_df, filename, repeating_num):
     # Create a list to hold new rows
     new_rows = []
     
-    for i in range(repeating_num):
-        jaw_angle = deepcopy(last_row["psm1_jaw"])
+    jaw_angle = deepcopy(last_row["psm1_jaw"])
+    jaw_angle_sp = deepcopy(last_row["psm1_jaw_sp"])
+    if jaw_angle >= -0.3:
 
-        if jaw_angle > 0:
-            last_row["psm1_jaw"] = jaw_angle - closing_rate * i
-            # print(jaw_angle - closing_rate * i)
-        else:
-            last_row["psm1_jaw"] = closing_angle
-            # print(closing_angle)
-        
-        new_rows.append(last_row.copy())
-
-    # Convert new rows to a DataFrame
-    new_rows_df = pd.DataFrame(new_rows)
+        for i in range(repeating_num):
+            jaw_angle = deepcopy(last_row["psm1_jaw"])
     
-    # Concatenate the original data and new rows
-    csv_df = pd.concat([csv_df, new_rows_df], ignore_index=True)
+            if jaw_angle > 0:
+                last_row["psm1_jaw_sp"] = jaw_angle - closing_rate * i
+                # print(jaw_angle - closing_rate * i)
+            else:
+                last_row["psm1_jaw_sp"] = closing_angle
+                # print(closing_angle)
+        
+            new_rows.append(last_row.copy())
+
+        # Convert new rows to a DataFrame
+        new_rows_df = pd.DataFrame(new_rows)
+    
+        # Concatenate the original data and new rows
+        csv_df = pd.concat([csv_df, new_rows_df], ignore_index=True)
+        csv_df.to_csv(filename, index=False)
+
+        # input("enter to continue...")
+
+def generate_cutting_motion_sp(csv_df, filename, repeating_num):
+    # Define the constants
+    closing_angle = -0.349096
+    closing_rate = 0.5
+
+    # Update the last 10 rows of 'psm1_jaw_sp'
+    for i in range(2, repeating_num+1):
+        csv_df.loc[csv_df.index[-i], 'psm1_jaw_sp'] = max(csv_df.loc[csv_df.index[-i+1], 'psm1_jaw'] - 0.1, closing_angle)
+
+    csv_df.loc[csv_df.index[-1], 'psm1_jaw_sp'] = max(csv_df.loc[csv_df.index[-1], 'psm1_jaw'] - 0.1, closing_angle)
+
+    # Save the updated DataFrame to a new CSV file
     csv_df.to_csv(filename, index=False)
 
-    # input("enter to continue...")
+def remove_extra_row(csv_df, filename, sample_path, repeating_num):
+    image_dir = os.path.join(sample_path, "left_img_dir")
+    image_list = os.listdir(image_dir)
+    
+    # Count the number of images
+    num_images = len(image_list)
+    
+    # Calculate the threshold for removing redundant rows
+    threshold = num_images + repeating_num
+    
+    # If the number of rows in the DataFrame exceeds the threshold, remove the redundant rows
+    if len(csv_df) > threshold:
+        print(f"Removing redundant rows from {filename}, csv length: {len(csv_df)}, num of img: {num_images}")
+        csv_df = csv_df.iloc[:threshold]
 
+    csv_df.to_csv(filename, index=False)
 
 
 if __name__ == "__main__":
-    tissue_ids = [1, 2]
+    # tissue_ids = [12, 13, 14, 18]
+    # tissue_ids = [1]
     # dataset_path = "/home/imerse/chole_ws/data/phantom_chole/phantom_1/ACTUAL_CUTTING_right"
-    phase = "16_go_to_the_cutting_position_right_tube_recovery"
+    # phase = "8_go_to_the_cutting_position_left_tube_recovery"
+    phase = "16_go_to_the_cutting_position_right_tube"
     for tissue_id in tissue_ids:
         dataset_path = f"/home/imerse/chole_ws/data/phantom_chole/phantom_{tissue_id}/{phase}"
+        # dataset_path = f"/home/imerse/chole_ws/data/base_chole_clipping_cutting/tissue_{tissue_id}/{phase}"
         samples = os.listdir(dataset_path)
         for sample in samples:
             sample_dir = os.path.join(dataset_path, sample)
+            if sample == "Corrections":
+                s = os.listdir(sample_dir)
+                for ss in s:
+                    sample_dir = os.path.join(sample_dir, ss)
+                    break
 
             if not os.path.exists(os.path.join(sample_dir, "ee_csv.csv")):
                 print(f"ee state csv file not found in {sample_dir}")
@@ -84,13 +126,16 @@ if __name__ == "__main__":
             header_name_qpos_psm1 = ["psm1_pose.position.x", "psm1_pose.position.y", "psm1_pose.position.z",
                                 "psm1_pose.orientation.x", "psm1_pose.orientation.y", "psm1_pose.orientation.z", "psm1_pose.orientation.w",
                                 "psm1_jaw"]
-            
+            header_name_actions_psm1 = ["psm1_sp.position.x", "psm1_sp.position.y", "psm1_sp.position.z",
+                            "psm1_sp.orientation.x", "psm1_sp.orientation.y", "psm1_sp.orientation.z", "psm1_sp.orientation.w",
+                            "psm1_jaw_sp"]
             episode_len = len(csv)
             # print("episode_len: ", episode_len)
             qpos_psm1 = csv[header_name_qpos_psm1].iloc[:, 0:episode_len].to_numpy()
             # print(qpos_psm1.shape)
             # visualize_robot_trajectory(qpos_psm1)
-            generate_cutting_motion(csv, csv_path, 10)
+            generate_cutting_motion_sp(csv, csv_path, 10)
+            # remove_extra_row(csv, csv_path, sample_dir, 10)
         print(f"Done for tissue {tissue_id}")
 
     print("job finished")
